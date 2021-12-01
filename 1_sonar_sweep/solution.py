9f451001124598b9aba980447ad2a98f367a53a5
Alan Grgic <alan.grgic@gmail.com>
2021-12-01 12:36:38 -0600

    day 1

--- 1_sonar_sweep/solution.py
+++ 1_sonar_sweep/solution.py
@@ -4,7 +4,6 @@ input_file[-1] = "input.txt"
 with open("/".join(input_file)) as f:
     nums = [int(entry.strip()) for entry in f]
 
-curr_sum = 0
 part_one = part_two = 0
 
 for i in range(len(nums)):
@@ -12,14 +11,8 @@ for i in range(len(nums)):
     if i > 0 and nums[i] > nums[i-1]:
         part_one += 1
 
-    prev_sum = curr_sum
-    curr_sum += nums[i]
-
-    if i > 2:
-        curr_sum -= nums[i-3]
-
-        if curr_sum > prev_sum:
-            part_two += 1
+    if i > 2 and nums[i] > nums[i-3]:
+        part_two += 1
 
 print(f"Part one: {part_one}")
 print(f"Part two: {part_two}")
